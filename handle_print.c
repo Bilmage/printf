da09fd69ee8b156298198fed1969649b0428883f
morris <mnjoroge25439@gmail.com>
2023-07-25 10:50:20 -0700

    printf:team project

--- handle_print.c
+++ handle_print.c
@@ -11,12 +11,6 @@
  * @size: Size specifier
  * Return: 1 or 2;
  */
-
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
